224403412b54b5f3c86ed9092c7e956d71b7f60c
Alexandru Margoi <alexandru.margoi@outlook.com>
2023-02-01 22:40:35 +0100

    Working Version

--- autoscout24/autoscout24/spiders/autoscout24.py
+++ autoscout24/autoscout24/spiders/autoscout24.py
@@ -18,13 +18,13 @@ import socket
 import uuid
 
 
-class Autoscout24Spider(scrapy.Spider):
+class Autoscout24Spider2(scrapy.Spider):
     MAX_PAGES = 20
-    name = 'autoscout24'
-    allowed_domains = ['web', 'autoscout24.com']
-    start_urls = ['https://www.autoscout24.com/lst?&sort=age&desc=1&ustate=N%2CU&size=20&cy=D&atype=C&page=1']
+    name = 'autoscout24v2'
+    allowed_domains = ['autoscout24.com']
+    start_urls = ['https://www.autoscout24.com/']
 
-    def parse(self, response):
+    def start_requests(self):
         page_count = 20
         # Get the next index URLs and yield Requests
         for make in Make:
@@ -32,7 +32,7 @@ class Autoscout24Spider(scrapy.Spider):
                 for sortcrit in SortCriteria:
                     for desc in SortDirection:
                         for idx in range(1, page_count+1):                    
-                            yield Request(
+                            yield scrapy.Request(
                                 'https://www.autoscout24.com/lst'
                                 + make.value
                                 + model.value
@@ -64,23 +64,21 @@ class Autoscout24Spider(scrapy.Spider):
                                 + PriceFrom.FROM_8000.value
                                 + '&atype=C&page='
                                 + str(idx)
-                            )
-
-        # Get item URLs and yield Requests
-        item_selector = response.xpath('//*[@class="cldt-summary-titles"]/a/@href')
-        for url in item_selector.extract():
-            yield Request(urljoin(response.url, url), callback=self.parse_item)
-
-    def parse_item(self, response):
-        """ This function parses a car page.
-        @url https://www.autoscout24.com/offers/ferrari-monza-sp2-ready-now-export-worldwide-gasoline-red-662f3a46-7168-46e5-b284-6636e5276303?cldtidx=1&cldtsrc=listPage
-        @returns items 1
-        @scrapes make model version registration price_euro driven_km power_kW
-        @scrapes url project spider server date
-        """
-
-        self.log("Visited %s" % response.url)
-        for car in response.selector.xpath("//main[@class='ListPage_main__L0gsf']/article").get():
+                            , callback=self.parse)
+        
+    def parse(self, response):
+        for index, car in enumerate(response.xpath("//main[@class='ListPage_main__L0gsf']/article")):
             yield {
-                'Model' : car.selector.xpath("/div[@class='ListItem_header__uPzec']//h2/text()").get(), 
+                'Index': index,
+                'Model': car.xpath(".//*[@class='ListItem_title__znV2I Link_link__pjU1l']/h2/text()").get(),
+                'Details': car.xpath(".//*[@class='ListItem_title__znV2I Link_link__pjU1l']/span[@class='ListItem_version__jNjur']/text()").get(),
+                'Price': ((car.xpath(".//p[@class='Price_price__WZayw']/text()").get()).replace("€ ", "")).replace(".-",""),
+                'Km' : (car.xpath(".//span[contains(text(), 'km')]/text()").get()).replace(" km", ""),
+                'FirstReg' : car.xpath(".//span[contains(text(), '/20')]/text()").get(),
+                'Power' : car.xpath(".//span[contains(text(), 'hp')]/text()").get(),
+                'Owners' : (car.xpath(".//span[contains(@class, 'VehicleDetailTable_item__koEV4')][5]/text()").get()).replace("- (Previous Owners)", "0 Previous Owners"),
+                'Transmission' : car.xpath(".//span[contains(@class, 'VehicleDetailTable_item__koEV4')][6]/text()").get(),
+                'Fuel' : car.xpath(".//span[contains(@class, 'VehicleDetailTable_item__koEV4')][7]/text()").get(),
+                'FuelConsumption' : car.xpath(".//span[contains(@class, 'VehicleDetailTable_item__koEV4')][8]/text()").get(),
             }
+            
